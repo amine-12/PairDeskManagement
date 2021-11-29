d9c27fc55436f5332f02f578f0e35d4d9c473d3c
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-11-29 16:40:04 -0500

    first commit

---
+++ src/main/java/com/project/pairdesksystem/PairdesksystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.pairdesksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairdesksystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PairdesksystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/pairdesksystem/PairdesksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pairdesksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairdesksystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
