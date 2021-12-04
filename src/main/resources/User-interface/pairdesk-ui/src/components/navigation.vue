4fd824f42d4facd803d73df5169a1c1b5142f800
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-12-04 11:47:33 -0500

    setup base ui (nav side bar component)

---
+++ src/main/resources/User-interface/pairdesk-ui/src/components/navigation.vue
@@ -0,0 +1,410 @@
+<template>
+  <nav class="navbar-personalized" style="z-index: 1; background-color: #23232e;">
+    <ul class="navbar-nav">
+      <li class="logo">
+        <a href="" class="nav-link">
+          <span class="link-text logo-text">MENU</span>
+          <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="angle-double-right"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              class="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+                  class="fa-secondary"
+              ></path>
+              <path
+                  fill="currentColor"
+                  d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+                  class="fa-primary"
+              ></path>
+            </g>
+          </svg>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="./overview.html" class="nav-link">
+          <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="cat"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="svg-inline--fa fa-cat fa-w-16 fa-9x"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M16.871 4.312c-.282-.764-1.01-1.312-1.871-1.312-1.298 0-2.313 1.244-1.904 2.582 1.111-.395 2.346-1.103 3.775-1.27zm-3.871 8.688c0-.552-.447-1-1-1s-1 .448-1 1 .447 1 1 1 1-.448 1-1zm5.318 4.873c1.463-.311 2.471-1.49 2.146-3.227 1.399 1.517-.155 3.949-2.146 3.227zm-14.001 0c1.463-.311 2.471-1.49 2.146-3.227 1.399 1.517-.156 3.949-2.146 3.227zm4.683-14.873c-.861 0-1.589.548-1.871 1.312 1.429.168 2.664.875 3.775 1.27.409-1.338-.606-2.582-1.904-2.582zm1.134 5.494c1.208.396 2.517.398 3.731 0 2.615-.854 3.515-1.986 4.452-.331.392.691 1.332 2.337 1.665 2.934-2.854-.575-5.572 1.416-5.94 4.254-1.233-.62-2.794-.647-4.084 0-.369-2.839-3.086-4.829-5.941-4.254.414-.738 1.183-2.081 1.665-2.933.936-1.655 1.834-.526 4.452.33zm7.171-3.175c-1.393 0-2.86.881-4.062 1.274-.812.266-1.685.263-2.486 0-1.201-.393-2.669-1.274-4.062-1.274-.998 0-1.958.452-2.754 1.859-2.406 4.255-3.941 6.687-3.941 8.822 0 2.761 2.238 5 5 5 4.039 0 4.156-4.123 7-4.123s2.961 4.123 7 4.123c2.762 0 5-2.239 5-5 0-2.135-1.535-4.567-3.941-8.821-.797-1.408-1.756-1.86-2.754-1.86zm1.695 13.681c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3-1.346 3-3 3zm-14 0c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3-1.346 3-3 3z"
+                  class="fa-secondary"
+              ></path>
+            </g>
+          </svg>
+          <span class="link-text">OVERVIEW</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="./settings.html" class="nav-link">
+          <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="space-shuttle"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="svg-inline--fa fa-space-shuttle fa-w-20 fa-5x"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M24 14.187v-4.374c-2.148-.766-2.726-.802-3.027-1.529-.303-.729.083-1.169 1.059-3.223l-3.093-3.093c-2.026.963-2.488 1.364-3.224 1.059-.727-.302-.768-.889-1.527-3.027h-4.375c-.764 2.144-.8 2.725-1.529 3.027-.752.313-1.203-.1-3.223-1.059l-3.093 3.093c.977 2.055 1.362 2.493 1.059 3.224-.302.727-.881.764-3.027 1.528v4.375c2.139.76 2.725.8 3.027 1.528.304.734-.081 1.167-1.059 3.223l3.093 3.093c1.999-.95 2.47-1.373 3.223-1.059.728.302.764.88 1.529 3.027h4.374c.758-2.131.799-2.723 1.537-3.031.745-.308 1.186.099 3.215 1.062l3.093-3.093c-.975-2.05-1.362-2.492-1.059-3.223.3-.726.88-.763 3.027-1.528zm-4.875.764c-.577 1.394-.068 2.458.488 3.578l-1.084 1.084c-1.093-.543-2.161-1.076-3.573-.49-1.396.581-1.79 1.693-2.188 2.877h-1.534c-.398-1.185-.791-2.297-2.183-2.875-1.419-.588-2.507-.045-3.579.488l-1.083-1.084c.557-1.118 1.066-2.18.487-3.58-.579-1.391-1.691-1.784-2.876-2.182v-1.533c1.185-.398 2.297-.791 2.875-2.184.578-1.394.068-2.459-.488-3.579l1.084-1.084c1.082.538 2.162 1.077 3.58.488 1.392-.577 1.785-1.69 2.183-2.875h1.534c.398 1.185.792 2.297 2.184 2.875 1.419.588 2.506.045 3.579-.488l1.084 1.084c-.556 1.121-1.065 2.187-.488 3.58.577 1.391 1.689 1.784 2.875 2.183v1.534c-1.188.398-2.302.791-2.877 2.183zm-7.125-5.951c1.654 0 3 1.346 3 3s-1.346 3-3 3-3-1.346-3-3 1.346-3 3-3zm0-2c-2.762 0-5 2.238-5 5s2.238 5 5 5 5-2.238 5-5-2.238-5-5-5z"
+                  class="fa-secondary"
+              ></path>
+
+            </g>
+          </svg>
+          <span class="link-text">FEATURES</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="./transactions.html" class="nav-link">
+          <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="space-shuttle"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="svg-inline--fa fa-space-shuttle fa-w-20 fa-5x"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4 14.083c0-2.145-2.232-2.742-3.943-3.546-1.039-.54-.908-1.829.581-1.916.826-.05 1.675.195 2.443.465l.362-1.647c-.907-.276-1.719-.402-2.443-.421v-1.018h-1v1.067c-1.945.267-2.984 1.487-2.984 2.85 0 2.438 2.847 2.81 3.778 3.243 1.27.568 1.035 1.75-.114 2.011-.997.226-2.269-.168-3.225-.54l-.455 1.644c.894.462 1.965.708 3 .727v.998h1v-1.053c1.657-.232 3.002-1.146 3-2.864z"
+                  class="fa-secondary"
+              ></path>
+            </g>
+          </svg>
+          <span class="link-text">INVOICES</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="./budget.html" class="nav-link">
+          <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="space-station-moon-alt"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="svg-inline--fa fa-space-station-moon-alt fa-w-16 fa-5x"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M7 19h-6v-11h6v11zm8-18h-6v18h6v-18zm8 11h-6v7h6v-7zm1 9h-24v2h24v-2z"
+                  class="fa-secondary"
+              ></path>
+            </g>
+          </svg>
+          <span class="link-text">PROGRESS</span>
+        </a>
+      </li>
+
+
+
+      <li class="nav-item">
+        <a href="./profile.html" class="nav-link">
+          <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="alien-monster"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="svg-inline--fa fa-alien-monster fa-w-18 fa-9x"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7.753 18.305c-.261-.586-.789-.991-1.871-1.241-2.293-.529-4.428-.993-3.393-2.945 3.145-5.942.833-9.119-2.489-9.119-3.388 0-5.644 3.299-2.489 9.119 1.066 1.964-1.148 2.427-3.393 2.945-1.084.25-1.608.658-1.867 1.246-1.405-1.723-2.251-3.919-2.251-6.31 0-5.514 4.486-10 10-10s10 4.486 10 10c0 2.389-.845 4.583-2.247 6.305z"
+                  class="fa-secondary"
+              ></path>
+            </g>
+          </svg>
+          <span class="link-text">USERS</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="./add-accounts.html" class="nav-link">
+          <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              data-icon="space-station-moon-alt"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              class="svg-inline--fa fa-space-station-moon-alt fa-w-16 fa-5x"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"
+                  class="fa-secondary"
+              ></path>
+
+            </g>
+          </svg>
+          <span class="link-text">ADD ACCOUNTS</span>
+        </a>
+      </li>
+
+
+
+
+      <li class="nav-item" >
+        <a href="../index.html" class="nav-link">
+          <svg
+              id="darkIcon"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fad"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+          >
+            <g class="fa-group">
+              <path
+                  fill="currentColor"
+                  d="M255.15,468.625H63.787c-11.737,0-21.262-9.526-21.262-21.262V64.638c0-11.737,9.526-21.262,21.262-21.262H255.15    c11.758,0,21.262-9.504,21.262-21.262S266.908,0.85,255.15,0.85H63.787C28.619,0.85,0,29.47,0,64.638v382.724    c0,35.168,28.619,63.787,63.787,63.787H255.15c11.758,0,21.262-9.504,21.262-21.262    C276.412,478.129,266.908,468.625,255.15,468.625z"
+                  class="fa-secondary"
+              ></path>
+              <path
+                  fill="currentColor"
+                  d="M505.664,240.861L376.388,113.286c-8.335-8.25-21.815-8.143-30.065,0.213s-8.165,21.815,0.213,30.065l92.385,91.173    H191.362c-11.758,0-21.262,9.504-21.262,21.262c0,11.758,9.504,21.263,21.262,21.263h247.559l-92.385,91.173    c-8.377,8.25-8.441,21.709-0.213,30.065c4.167,4.21,9.653,6.336,15.139,6.336c5.401,0,10.801-2.041,14.926-6.124l129.276-127.575    c4.04-3.997,6.336-9.441,6.336-15.139C512,250.302,509.725,244.88,505.664,240.861z"
+                  class="fa-primary"
+              ></path>
+            </g>
+          </svg>
+          <span class="link-text">Log out</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navigation'
+}
+</script>
+
+<style>
+:root {
+  font-size: 16px;
+  font-family: 'Open Sans';
+  --text-primary: #b6b6b6;
+  --text-secondary: #ececec;
+  --bg-primary: #23232e;
+  --bg-secondary: #141418;
+  --transition-speed: 600ms;
+}
+
+body {
+  color: black;
+  background-color: rgb(226, 226, 226);
+  margin: 0;
+  padding: 0;
+}
+
+body::-webkit-scrollbar {
+  width: 0.5rem;
+}
+
+body::-webkit-scrollbar-track {
+  background: #1e1e24;
+}
+
+body::-webkit-scrollbar-thumb {
+  background: #6649b8;
+}
+
+main {
+  margin-left: 5rem;
+  padding: 1rem;
+}
+
+.navbar-personalized {
+  position: fixed;
+  background-color: var(--bg-primary);
+  transition: width 600ms ease;
+  overflow: hidden;
+}
+
+.navbar-nav {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+}
+
+.nav-item {
+  width: 100%;
+}
+
+.nav-item:last-child {
+  margin-top: auto;
+}
+
+.nav-link {
+  display: flex;
+  align-items: center;
+  height: 5rem;
+  color: var(--text-primary);
+  text-decoration: none;
+  filter: grayscale(100%) opacity(0.7);
+  transition: var(--transition-speed);
+}
+
+.nav-link:hover {
+  filter: grayscale(0%) opacity(1);
+  background: var(--bg-secondary);
+  color: var(--text-secondary);
+}
+
+.link-text {
+  display: none;
+  margin-left: 1rem;
+}
+
+.nav-link svg {
+  width: 2rem;
+  min-width: 2rem;
+  margin: 0 1.5rem;
+}
+
+.fa-primary {
+  color: #f19355;
+}
+
+.fa-secondary {
+  color: #f19355;
+}
+
+.fa-primary,
+.fa-secondary {
+  transition: var(--transition-speed);
+}
+
+.logo {
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+  text-align: center;
+  color: var(--text-secondary);
+  background: var(--bg-secondary);
+  font-size: 1.5rem;
+  letter-spacing: 0.3ch;
+  width: 100%;
+}
+
+.logo svg {
+  transform: rotate(0deg);
+  transition: var(--transition-speed);
+}
+
+.logo-text
+{
+  display: inline;
+  position: absolute;
+  left: -999px;
+  transition: var(--transition-speed);
+}
+
+.navbar-personalized:hover .logo svg {
+  transform: rotate(-180deg);
+}
+
+/* Small screens */
+@media only screen and (max-width: 600px) {
+  .navbar-personalized {
+    overflow: scroll ;
+    bottom: 0;
+    width: 100vw;
+    height: 5rem;
+  }
+
+  .logo {
+    display: none;
+  }
+
+  .navbar-nav {
+    flex-direction: row;
+  }
+
+  .nav-link {
+    justify-content: center;
+  }
+
+  main {
+    margin: 0;
+  }
+}
+
+/* Large screens */
+@media only screen and (min-width: 600px) {
+  .navbar-personalized {
+    top: 0;
+    width: 5rem;
+    height: 100vh;
+  }
+
+  .navbar-personalized:hover {
+    width: 16rem;
+  }
+
+  .navbar-personalized:hover .link-text {
+    display: inline;
+  }
+
+  .navbar-personalized:hover .logo svg
+  {
+    margin-left: 11rem;
+  }
+
+  .navbar-personalized:hover .logo-text
+  {
+    left: 0px;
+  }
+}
+
+
+
+.title{
+  font-size: 3rem;
+  margin: 0;
+  text-transform: uppercase;
+}
+</style>
+
