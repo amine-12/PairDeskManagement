5d614a03a7767aab75212d07ba85642bacb6a05f
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-12-04 11:47:33 -0500

    setup base ui (nav side bar component)

--- src/main/resources/User-interface/pairdesk-ui/src/App.vue
+++ src/main/resources/User-interface/pairdesk-ui/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navigation />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/navigation.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
