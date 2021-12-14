539d9473f8e1ff28229840ece6e70e4c07099fcd
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-12-14 14:45:02 -0500

    floating button design

---
+++ src/main/resources/User-interface/pairdesk-ui/src/components/FloatingButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <a style="padding:10px;display:block;" href="http://androidcss.com/css/css3-floating-button-tutorial/" target="_blank">Click here for complete tutorial</a>
+
+  <!-- Code begins here -->
+
+  <a href="#" class="float">
+    <i class="fa fa-plus my-float"></i>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "FloatingButton"
+}
+</script>
+
+<style scoped>
+*{padding:0;margin:0;}
+
+body{
+  font-family:Verdana, Geneva, sans-serif;
+  font-size:18px;
+  background-color:#CCC;
+}
+
+.float{
+  position:fixed;
+  width:60px;
+  height:60px;
+  bottom:40px;
+  right:40px;
+  background-color:#0C9;
+  color:#FFF;
+  border-radius:50px;
+  text-align:center;
+  box-shadow: 2px 2px 3px #999;
+}
+
+.my-float{
+  margin-top:22px;
+}
+</style>
