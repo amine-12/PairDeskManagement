98d516e38fa75c2749fa33f72c9e76f991e62efc
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-12-15 11:28:07 -0500

    refactored unit tests

--- src/test/java/com/project/pairdesksystem/PairdesksystemApplicationTests.java
+++ src/test/java/com/project/pairdesksystem/PairdesksystemApplicationTests.java
@@ -20,47 +20,5 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PairdesksystemApplicationTests {
 
-    @Autowired
-    private FeaturesRepository featRep;
-
-
-    @BeforeEach
-    public void setUpDb(){
-
-        Feature f1 = new Feature();
-            Date date = new Date();
-
-
-            f1.setId(1);
-            f1.setFeatureName("MyFeature");
-            f1.setDeadline(date);
-            f1.setDescription("Some Feature here");
-            f1.setPriority("MEDIUM");
-            f1.setUser_id(1);
-            f1.setProgress(2);
-        featRep.save(f1);
-        Feature f2 = new Feature();
-
-            f2.setId(2);
-            f2.setFeatureName("MyFeature2");
-            f2.setDeadline(date);
-            f2.setDescription("Some Feature here");
-            f2.setPriority("MEDIUM");
-            f2.setUser_id(1);
-            f2.setProgress(2);
-            featRep.save(f2);
-    }
-
-    @Test
-    void saveFeatures() {
-        assertEquals( featRep.count(), 2);
-    }
-
-    @Test
-    void get_All_Features(){
-        featRep.findAll();
-        assertEquals(featRep.findAll().size(), 2);
-
-    }
 
 }
