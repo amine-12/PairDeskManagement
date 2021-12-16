6d925769e3c440aade624bfbd2573483a7e242c5
christopherl04 <christopherlombart4@gmail.com>
2021-12-16 14:34:32 -0500

    removed the useless autowired annotation for the service.

--- src/test/java/com/project/pairdesksystem/PairdesksystemApplicationTests.java
+++ src/test/java/com/project/pairdesksystem/PairdesksystemApplicationTests.java
@@ -10,24 +10,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-
 import java.util.Date;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 @DataJpaTest
-
-
 public class PairdesksystemApplicationTests {
 
     @Autowired
     private FeaturesRepository featRep;
-
-    @Autowired
-    FeatureService featureService;
-
 
     @BeforeEach
     public void setUpDb(){
