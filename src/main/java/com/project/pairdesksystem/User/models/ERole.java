b5c68a78f66e81fc2d076c49921c0dfa26c021c8
amine-12 <54552812+amine-12@users.noreply.github.com>
2022-01-10 11:11:01 -0500

    role and users model

---
+++ src/main/java/com/project/pairdesksystem/User/models/ERole.java
@@ -0,0 +1,6 @@
+package com.project.pairdesksystem.User.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/project/pairdesksystem/User/models/UserDTO.java
@@ -0,0 +1,60 @@
+package com.project.pairdesksystem.User.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+    private Set<Role> roles = new HashSet<>();
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
