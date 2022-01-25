06285d05f345a946a496d3fbceafecf05237d4c3
AndrianZberea <andrian.zbere@gmail.com>
2022-01-25 18:49:35 -0500

    unit test for feature status

--- src/test/java/com/project/pairdesksystem/FeatureAPITests.java
+++ src/test/java/com/project/pairdesksystem/FeatureAPITests.java
@@ -2,6 +2,7 @@ package com.project.pairdesksystem;
 
 import com.project.pairdesksystem.datalayer.Feature.Feature;
 import com.project.pairdesksystem.datalayer.Feature.FeaturesRepository;
+import javassist.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,6 +64,34 @@ public class FeatureAPITests {
     void get_Features_By_Id(){
         Feature feat = featRep.findById(1).get();
         assertEquals(feat.getFeatureName(), "MyFeature");
+    }
+
+    @Test
+    void update_Feature_With_Service() throws NotFoundException {
+        Feature f1 = new Feature();
+        Date date = new Date();
+
+
+        f1.setId(1);
+        f1.setFeatureName("MyFeature");
+        f1.setDeadline(date);
+        f1.setDescription("Some Feature here");
+        f1.setPriority("MEDIUM");
+        f1.setUserId(1);
+        featRep.save(f1);
+        Feature f2 = new Feature();
+
+        f2.setId(2);
+        f2.setFeatureName("MyFeature2");
+        f2.setDeadline(date);
+        f2.setDescription("Some Feature here");
+        f2.setPriority("MEDIUM");
+        f2.setUserId(1);
+        featRep.save(f2);
+
+        assertEquals(f2.getPriority(), "MEDIUM");
+        featRep.deleteAll();
+
     }
 
 }
