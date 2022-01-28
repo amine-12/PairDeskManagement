23252b68d8f40230f915d413f1d5accc12d73c2a
christopherl04 <christopherlombart4@gmail.com>
2022-01-28 11:45:35 -0500

    using the user's download folder instead of desktop to be more portable.

--- src/main/java/com/project/pairdesksystem/buinesslayer/Reports/ReportService.java
+++ src/main/java/com/project/pairdesksystem/buinesslayer/Reports/ReportService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import sun.security.util.Resources;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
@@ -34,14 +36,26 @@ public class ReportService {
         JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
         String userDesktopPath = System.getProperty("user.home") + "/Desktop";
+        String userDownloadsPath = System.getProperty("user.home") + "/Downloads";
+
+//        JLabel l = new JLabel("No location selected");
+//
+//        JFileChooser jFileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+//        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+//
+//        int r = jFileChooser.showSaveDialog(null);
+//
+//        if (r == JFileChooser.APPROVE_OPTION) {
+//            l.setText(jFileChooser.getSelectedFile().getAbsolutePath());
+//        }
 
         if(reportFormat.equalsIgnoreCase("html")){
-            JasperExportManager.exportReportToHtmlFile(jasperPrint, userDesktopPath + "\\featuresSummaryReport.html");
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, userDownloadsPath + "\\featuresSummaryReport.html");
         }
         if(reportFormat.equalsIgnoreCase("pdf")){
-            JasperExportManager.exportReportToPdfFile(jasperPrint, userDesktopPath + "\\featuresSummaryReport.pdf");
+            JasperExportManager.exportReportToPdfFile(jasperPrint, userDownloadsPath + "\\featuresSummaryReport.pdf");
         }
 
-        return "Report was generated and saved on the desktop";
+        return "Report was generated and saved in your downloads folder";
     }
 }
