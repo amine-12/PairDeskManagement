5b1263fb46c34f805a9c41ce5a6a0d458e3c703b
AndrianZberea <andrian.zbere@gmail.com>
2022-01-28 12:24:33 -0500

    placed flags to fit UI better

--- src/main/resources/User-interface/pairdesk-ui/src/App.vue
+++ src/main/resources/User-interface/pairdesk-ui/src/App.vue
@@ -1,25 +1,23 @@
 <template>
   <router-view/>
+
+  <!--<select style=" position: absolute;
+  top: 8px;
+  right: 16px;
+  font-size: 18px;" v-model="lang">
+    <option value="en">English</option>
+    <option value="fr">Francais</option>
+  </select>-->
+  <language-selector></language-selector>
+
 </template>
 
 <script>
+import languageSelector from "./components/languageSelector"
 export default {
-  name: 'Home'
+  name: 'Home',
+  components:{
+    languageSelector
+  }
 }
 </script>
-
-<style>
-::-webkit-scrollbar {
-  width: 10px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #2b3d8c;
-}
-</style>
---
+++ src/main/resources/User-interface/pairdesk-ui/src/components/languageSelector.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="position: absolute;
+  top: 8px;
+  right: 16px;
+  font-size: 18px;"
+       name="flags">
+
+
+    <span style="cursor:pointer" class="font-weight-light" @click="$i18n.locale = 'en'">English </span>
+    <img
+        src="https://flagcdn.com/16x12/gb.png"
+        srcset="https://flagcdn.com/32x24/gb.png 2x,
+    https://flagcdn.com/48x36/gb.png 3x"
+        width="16"
+        height="12"
+        alt="United Kingdom">
+    <span style="cursor:pointer; padding-left: 5px" class="font-weigh-light" @click="$i18n.locale = 'fr'">Francais </span>
+
+    <img
+        src="https://flagcdn.com/16x12/mf.png"
+        srcset="https://flagcdn.com/32x24/mf.png 2x,
+    https://flagcdn.com/48x36/mf.png 3x"
+        width="16"
+        height="12"
+        alt="Saint Martin">
+
+  </div>
+
+</template>
