e4e00b65abcf117980fe057c8de08f3354e5166e
christopherl04 <christopherlombart4@gmail.com>
2022-01-27 20:21:32 -0500

    Created the entire Reports page for all future report user stories. Created the reports service and resource for future report stories as well. You may now export a nice report in the form of a pdf from our app. The unit test passes as well. All routing has been done for the reports page to work well. Build gradle changes for Jasper Reports library that i added.

---
+++ src/main/resources/User-interface/pairdesk-ui/src/views/ReportsPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="nav">
+    <navigation/>
+  </div>
+  <div class="about">
+    <feature-summary></feature-summary>
+  </div>
+</template>
+
+<script>
+import Navigation from "@/components/navigation";
+import FeatureSummary from "@/components/ReportsPage/FeatureSummary";
+
+export default {
+  name: "ReportsPage",
+  components: {Navigation,FeatureSummary}
+}
+</script>
+
+<style scoped>
+
+</style>
